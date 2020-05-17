c9b730697072dcc06efaafcaf406a0e52b7c6ba4
Felipe Romero <feliperomero3@outlook.com>
2020-05-17 17:03:48 -0500

    Add exception handling to product service

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { IProduct } from './product';
 
 @Injectable({
@@ -12,7 +13,22 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>(this.productUrl);
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+      tap(data => console.log('All: ' + JSON.stringify(data))),
+      catchError(err => {
+        let errorMessage = '';
+        const errorResponse = err as HttpErrorResponse;
+        const errorResponseMessage = errorResponse.error.message;
+        if (errorResponse.error instanceof ErrorEvent) {
+          errorMessage = `An error ocurred: ${errorResponseMessage}`;
+        } else {
+          errorMessage =
+            `Server retured code: ${errorResponse.status}, error message is: ${errorResponseMessage}`;
+        }
+        console.error(errorMessage);
+        throw new Error(errorMessage);
+      })
+    );
   }
 
 }
