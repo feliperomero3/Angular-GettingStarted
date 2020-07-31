9ac5dcc4b79dcc78c389f3ec329b14ea7e010fe9
Felipe Romero <feliperomero3@outlook.com>
2020-07-30 23:20:41 -0500

    Add StarComponent test stub

---
+++ src/app/shared/star.component.spec.ts
@@ -0,0 +1,12 @@
+import { async, TestBed } from '@angular/core/testing';
+import { StarComponent } from './star.component';
+
+describe('StarComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        StarComponent
+      ]
+    }).compileComponents();
+  }));
+});
